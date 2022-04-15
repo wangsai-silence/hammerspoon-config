385bc1934e2ff77d3f1343e3c7092e21b0677c86
silence <wangsai.silence@qq.com>
2022-04-15 22:15:38 +0800

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,90 @@
+local log = hs.logger.new('wang','debug')
+-- Set hyper to ctrl + alt + cmd + shift
+local hyper      = {'ctrl', 'cmd', 'alt'}
+
+-- Move Mouse to center of next Monitor
+hs.hotkey.bind(hyper, 'right', function()
+    local screen = hs.mouse.getCurrentScreen()
+    local nextScreen = screen:toEast()
+
+    if not nextScreen then
+        return
+    end
+
+    local rect = nextScreen:fullFrame()
+    local center = hs.geometry.rectMidPoint(rect)
+ 
+    hs.mouse.absolutePosition(center)
+end)
+
+-- Move Mouse to center of previous Monitor
+hs.hotkey.bind(hyper, 'left', function()
+    local screen = hs.mouse.getCurrentScreen()
+    local previousScreen = screen:toWest()
+
+    if not previousScreen then 
+        return
+    end
+
+    local rect = previousScreen:fullFrame()
+    local center = hs.geometry.rectMidPoint(rect)
+ 
+    hs.mouse.absolutePosition(center)
+end)
+
+
+-- 将当前窗口移动到第 n 个屏幕
+-- 并最大化窗口
+local hyper2 = {'ctrl', 'cmd'}
+    
+-- hyper + shift + left move the current window to the left monitor
+hs.hotkey.bind(hyper2, 'left', function() 
+    local w = hs.window.focusedWindow()
+
+    if not w then 
+        return
+    end
+
+    local wSize = w:size()
+    log:d('window size', wSize.w)
+
+    w:moveOneScreenWest(false, true)
+end)
+
+-- hyper + shift + right move the current window to the right monitor
+hs.hotkey.bind(hyper2, 'right', function() 
+    local w = hs.window.focusedWindow()
+    if not w then 
+        return
+    end
+
+    w:moveOneScreenEast(false, true)
+end)
+
+hs.hotkey.bind(hyper2, 'up', function() 
+    local w = hs.window.focusedWindow()
+    if not w then 
+        return
+    end
+
+    local isFullScreen = w:isFullScreen()
+    if isFullScreen then
+        return
+    end
+
+    w:setFullScreen(true)
+end)
+
+hs.hotkey.bind(hyper2, 'down', function() 
+    local w = hs.window.focusedWindow()
+    if not w then 
+        return
+    end
+
+    local isFullScreen = w:isFullScreen()
+    if not isFullScreen then
+        return
+    end
+
+    w:setFullScreen(false)
+end)
